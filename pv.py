e94954d0f1f0b10c31ff2bced8353a2f020361bb
willrawlingsox <william.rawlings@stcatz.ox.ac.uk>
2024-10-30 09:51:08 +0000

    Update pv.py

--- pv.py
+++ pv.py
@@ -13,35 +13,42 @@ class Roof():
     def __init__(self, width, height, angle):
         self.width = width
         self.height = height
-        self.angle = m.radians(angle)
+        self.angle = angle
 
 
 class PV_Panel():
     def __init__(self, length, height, power):
-        self.length = length/1000
-        self.height = height/1000
+        self.length = length
+        self.height = height
         self.power = power
         
 
 roof = Roof(0,0,0)
-roof.width = int(input("Building Width: "))
-roof.height = int(input("Building Length: "))
-roof.angle = int(input("Roof Angle: "))
+roof.width = float(input("Building Width: "))
+roof.height = float(input("Building Length: "))
+roof.angle = float(input("Roof Angle: "))
 
 pv = PV_Panel(0,0,0)
-pv.length = int(input("Panel Length: "))
-pv.height = int(input("Panel Height: "))
-pv.power = int(input("Panel Power: "))
+pv.length = float(input("Panel Length: "))
+pv.height = float(input("Panel Height: "))
+pv.power = float(input("Panel Power: "))
 
-roof_length = roof.width/(m.cos(roof.angle))
-total_panels_width = width = 2*((roof_length/2)//pv.length)
+rads = m.radians(roof.angle)
+cos = m.cos(rads)
+print(rads,cos)
+
+roof_length = roof.width/cos
+print(roof_length)
+
+total_panels_width = m.trunc((2*((roof_length/2)/pv.length)))
+print(total_panels_width)
 total_panels_height = roof.height//(pv.height)
+print(total_panels_height)
 panels = total_panels_height*total_panels_width
+print(panels)
 capacity =pv.power*panels
 print(capacity)
                                         
-
-
 
     
         
